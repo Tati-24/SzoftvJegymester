d9ab6fcdc940bce2d0890e98ac0afcc7b4e5d5d5
Tóth Attila <atus200104@gmail.com>
2026-05-13 09:04:42 +0200

    Spelling fix and merge

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -43,7 +43,7 @@ namespace AspNetServer.Migrations
                     b.Property<bool>("IsActive")
                         .HasColumnType("tinyint(1)");
 
-                    b.Property<int>("Lengt")
+                    b.Property<int>("Length")
                         .HasColumnType("int");
 
                     b.Property<DateTime>("ReleaseDate")
